0c2a0f4cad5206d27c63c3baa8e948298a169881
sam bacha <sam@manifoldfinance.com>
2021-09-13 08:22:30 -0700

    nix update
    Signed-off-by: sam bacha <sam@manifoldfinance.com>

--- default.nix
+++ default.nix
@@ -1,1 +1,13 @@
-(import ./nix { }).ci
+ let
+   nix-pre-commit-hooks = import (builtins.fetchTarball "https://github.com/cachix/pre-commit-hooks.nix/tarball/master");
+ in {
+   pre-commit-check = nix-pre-commit-hooks.run {
+     src = ./.;
+     # If your hooks are intrusive, avoid running on each commit with a default_states like this:
+     # default_stages = ["manual" "push"];
+     hooks = {
+       prettier.enable = true;
+       shellcheck.enable = true;
+     };
+   };
+ }
