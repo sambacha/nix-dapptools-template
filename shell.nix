89003ab89cf7e79baf3eb3892567c72742b1fcc1
sam bacha <sam@manifoldfinance.com>
2021-09-13 07:26:15 -0700

    v0.1.0
    Signed-off-by: sam bacha <sam@manifoldfinance.com>

--- shell.nix
+++ shell.nix
@@ -1,9 +1,19 @@
-{ project ? import ./nix { }
-}:
+# nix
+let
+  pkgs = import (builtins.fetchGit rec {
+    name = "dapptools-${rev}";
+    url = https://github.com/dapphub/dapptools;
+    rev = "adcc076b1441b3a928a8f0b42b2a63f05d9bcf0d";
+  }) {};
 
-project.pkgs.mkShell {
-  buildInputs = builtins.attrValues project.devTools;
-  shellHook = ''
-    ${project.ci.pre-commit-check.shellHook}
-  '';
+in
+  pkgs.mkShell {
+    src = null;
+    name = "nix-dapptools-boilerplate";
+    buildInputs = with pkgs; [
+      pkgs.dapp
+      pkgs.seth
+      pkgs.go-ethereum-unlimited
+      pkgs.hevm
+    ];
 }
